36d71b0b39c99f81c66e00134a1a974229515b19
Rushi <Rushi@LAPTOP-26H45Q5R>
2020-10-12 17:57:38 +0530

    new new new new new

---
+++ main assignment/Hello/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+	public static void main(String Args[]) {
+		System.out.println("HEllo Rushi");
+		System.out.println("HEllo Rushi");
+		System.out.println("HEllo Rushi");
+		
+	}
+
+}
